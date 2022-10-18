4349e8c97be6e6ed77b81fa4ebd570ab02c17612
Nataly Zernova <nz_18@mail.ru>
2022-10-18 19:33:24 +0300

    solve 'recursive-depth' task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -3,9 +3,9 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class DepthCalculator with method calculateDepth
  * that calculates deoth of nested array
- * 
+ *
  * @example
- * 
+ *
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
@@ -13,11 +13,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(array) {
+    return Array.isArray(array)
+      ? 1 + Math.max(0, ...array.map(item =>this.calculateDepth(item)))
+      : 0;
   }
 }
+
 
 module.exports = {
   DepthCalculator
--- src/transform-array.js
+++ src/transform-array.js
@@ -3,19 +3,60 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(array) {
+  if (!(array instanceof Array)) {
+    throw new Error(`'arr' parameter must be an instance of the Array!`)
+  }
+  const operations = {
+    "--double-prev": (acc, index, arr) => {
+      if (index !== 0) {
+        return acc.concat(arr[index - 1]);
+      }
+
+      return acc;
+    },
+    "--double-next": (acc, index, arr) => {
+      if (index !== arr.length - 1) {
+        return acc.concat(arr[index + 1]);
+      }
+
+      return acc;
+    },
+    "--discard-prev": (acc, index) => {
+      if (index !== 0) {
+        acc.splice(index - 1, 1);
+
+        return acc;
+      }
+
+      return acc;
+    },
+    "--discard-next": (acc, index, arr) => {
+      if (index !== arr.length - 1) {
+        arr.splice(index + 1, 1);
+      }
+
+      return acc;
+    }
+  };
+
+  return array.slice().reduce((acc, item, index, arr) => {
+    if (operations[item]) {
+      return operations[item](acc, index, arr);
+    }
+
+    return acc.concat(item);
+  }, []);
 }
 
 module.exports = {
